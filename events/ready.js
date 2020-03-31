0054e17ed8bdaf4a4289d33485b475b491c6b1ad
noah427 <redacted427@gmail.com>
2020-03-31 17:23:26 -0400

    added ready message (please use klasa events @ddb)

---
+++ events/ready.js
@@ -0,0 +1,20 @@
+const { Event } = require("klasa");
+
+module.exports = class extends Event {
+  constructor(...args) {
+    super(...args, { name: "ready", enabled: true });
+  }
+
+  run() {
+    this.client.user.setPresence({
+      status: "dnd",
+      activity: {
+        type: "WATCHING",
+        name: "The stock market crash."
+      }
+    });
+    console.log("Bot started");
+  }
+
+  async init() {}
+};
--- index.js
+++ index.js
@@ -8,20 +8,9 @@ var Klient = new Client({
   prefix: "$",
   cmdEditing: true,
   typing: true,
-  readyMessage: client =>
-    `${client.user.tag}, Ready to serve ${client.guilds.size} guilds and ${client.users.size} users`
 })
 
-Klient.on("ready", () => {
 
-  Klient.user.setStatus("dnd");
-  console.log("Bot started");
-  
-        Klient.user.setActivity("The DOW Jones fall", {
-          url: "",
-          type: "WATCHING"
-        }); 
-})
 // run evaluation tasks on servers
 
 Klient.login(process.env.TOKEN);
