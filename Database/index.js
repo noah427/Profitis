5722a3abaa7476067707f3f119b4d6fee8724351
noah427 <redacted427@gmail.com>
2020-03-31 17:03:24 -0400

    database guild tools

--- Database/index.js
+++ Database/index.js
@@ -1,10 +1,19 @@
 const sql = require("sqlite3");
-var db = new sqlite3.Database('./database.db');
+var db = new sqlite3.Database("./database.db");
 
 db.serialize(function() {
+  db.run(
+    "CREATE TABLE IF NOT EXISTS guilds (guildID TEXT UNIQUE, guildName TEXT, guildMemberCount INTEGER)"
+  );
   // Create server worth table
 
   // Create user worth table
 });
+
+const addServerDB = db.prepare("INSERT INTO guilds (?,?,?)");
+
+module.exports.addServer = (id, name, memberCount) => {
+  addServerDB.run(id, name, memberCount)
+};
 
 db.close();
