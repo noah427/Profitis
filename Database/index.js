b139904f714513503055323028f9bd5b78cb4974
noah427 <redacted427@gmail.com>
2020-03-31 18:46:58 -0400

    added more guild tools for database

--- Database/index.js
+++ Database/index.js
@@ -1,19 +1,34 @@
 const sql = require("sqlite3");
-var db = new sqlite3.Database("./database.db");
+var db = new sql.Database("./database.db");
 
 db.serialize(function() {
   db.run(
-    "CREATE TABLE IF NOT EXISTS guilds (guildID TEXT UNIQUE, guildName TEXT, guildMemberCount INTEGER)"
+    "CREATE TABLE IF NOT EXISTS guilds (guildID TEXT UNIQUE, guildName TEXT, guildMemberCount INTEGER, guildValue INTEGER, guildRevenue INTEGER, guildOwnerID TEXT, guildSharesAvailable INTEGER, guildSharesOwned INTEGER, guildSharesTotal INTEGER)"
   );
   // Create server worth table
 
   // Create user worth table
 });
 
-const addServerDB = db.prepare("INSERT INTO guilds (?,?,?)");
+const addServerDB = db.prepare("INSERT INTO guilds (?,?,?,?,?,?)");
+const serverTopDB = db.prepare("SELECT * FROM guilds ORDER BY guildValue DESC");
 
-module.exports.addServer = (id, name, memberCount) => {
-  addServerDB.run(id, name, memberCount)
+module.exports.addServer = (id, name, memberCount, ownerID) => {
+  addServerDB.run(
+    String(id),
+    name,
+    memberCount,
+    0,
+    0,
+    String(ownerID),
+    0,
+    0,
+    0
+  );
+};
+
+module.exports.serverTop = cb => {
+  serverTopDB.all(list => cb(list));
 };
 
 db.close();
